175724dc13729328dc36edb3f6f9c41f62878899
Rehoboth Ini <rehobothokoibu@gmail.com>
2025-10-16 19:44:53 +0100

    ssr fix

--- Frontend/src/routes/nft/[id]/+page.svelte
+++ Frontend/src/routes/nft/[id]/+page.svelte
@@ -1,47 +1,53 @@
 <script>
-  import { onMount } from 'svelte';
-  import { page } from '$app/stores';
-  import { getNFT } from '$lib/api.js';
-  import { wallet } from '$lib/stores/wallet.js';
-  import BuyNFT from '$lib/components/BuyNFT.svelte';
-  import ListNFT from '$lib/components/ListNFT.svelte';
+	import { page } from '$app/stores';
+	import { getNFT } from '$lib/api.js';
+	import { wallet } from '$lib/stores/wallet.js';
+	import { browser } from '$app/environment';
+	import BuyNFT from '$lib/components/BuyNFT.svelte';
+	import ListNFT from '$lib/components/ListNFT.svelte';
 
-  let nft = null;
-  let isLoading = true;
-  let error = '';
+	let nft = $state(null);
+	let isLoading = $state(true);
+	let error = $state('');
+	let isOwner = $state(false);
 
-  const tokenId = $page.params.id;
+	const tokenId = $page.params.id;
 
-  onMount(async () => {
-    try {
-      nft = await getNFT(tokenId);
-    } catch (e) {
-      error = e.message;
-    } finally {
-      isLoading = false;
-    }
-  });
+	$effect(() => {
+		if (browser && tokenId) {
+			loadNFT();
+		}
+	});
 
-  let isOwner;
-  $: isOwner = nft && $wallet.stxAddress && nft.ownerAddress === $wallet.stxAddress;
+	async function loadNFT() {
+		isLoading = true;
+		error = '';
+		try {
+			nft = await getNFT(tokenId);
+			// Check ownership after NFT loads
+			isOwner = nft && $wallet.stxAddress && nft.ownerAddress === $wallet.stxAddress;
+		} catch (e) {
+			error = e.message;
+		} finally {
+			isLoading = false;
+		}
+	}
 </script>
 
 <section class="nft-detail-container">
-  {#if isLoading}
-    <p>Loading NFT details...</p>
-  {:else if error}
-    <p class="error-message">{error}</p>
-  {:else if nft}
-    <h1>NFT #{nft.tokenId.slice(0, 8)}...</h1>
-    <img src={nft.imageUrl} alt="NFT Artwork" />
-
-    <!-- Action Components -->
-    <div>
-      {#if isOwner && !nft.onChainListing}
-        <ListNFT {tokenId} />
-      {:else if !isOwner && nft.onChainListing}
-        <BuyNFT {tokenId} price={nft.onChainListing.price.value} />
-      {/if}
-    </div>
-  {/if}
+	{#if isLoading}
+		<p>Loading NFT details...</p>
+	{:else if error}
+		<p class="error-message">{error}</p>
+	{:else if nft}
+		<h1>NFT #{nft.tokenId.slice(0, 8)}...</h1>
+		<img src={nft.imageUrl} alt="NFT Artwork" />
+		<div>
+			{#if isOwner && !nft.onChainListing}
+				<ListNFT {tokenId} />
+			{:else if !isOwner && nft.onChainListing}
+				<BuyNFT {tokenId} price={nft.onChainListing.price.value} />
+			{/if}
+		</div>
+	{/if}
 </section>
